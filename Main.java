186b6a6e334ddcfa9335908ae0de5874d91670ee
Vinícius Vasconcelos Santos <129818517+Raspiitim@users.noreply.github.com>
2024-08-19 16:56:19 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,19 @@
 public class Main {
 
 	public static void main(String[] args) {
+		Pilha p1 = new Pilha(5);
 		
-			
+		System.out.println("Tamanho da pilha: " + p1.size());
+		System.out.println("Inserindo na pilha: " + p1.push(10));
+		System.out.println("Inserindo na pilha: " + p1.push(20));
+		System.out.println("Inserindo na pilha: " + p1.push(30));
+		System.out.println("Topo da pilha: " + p1.top());
+		System.out.println("Removendo o topo: " + p1.pop());
+		System.out.println("Topo da pilha: " + p1.pop());
+		System.out.println("Tamanho da pilha: " + p1.size());
+		System.out.println("Topo da pilha: " + p1.top());
+		
+		
 	}
 
 }
